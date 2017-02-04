b8bf25f429fbc5524a723264ef6f65a240cef7e5
vahes <vahes@DESKTOP-GKCT3MD>
2017-02-04 20:26:25 +0530

    Add project files.

---
+++ Arduino_Test/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Arduino_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSendA = new System.Windows.Forms.Button();
+            this.btnSendB = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnUpdate1 = new System.Windows.Forms.Button();
+            this.btnUpdate2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSendA
+            // 
+            this.btnSendA.Location = new System.Drawing.Point(13, 53);
+            this.btnSendA.Name = "btnSendA";
+            this.btnSendA.Size = new System.Drawing.Size(75, 23);
+            this.btnSendA.TabIndex = 2;
+            this.btnSendA.Text = "Recieve 1";
+            this.btnSendA.UseVisualStyleBackColor = true;
+            this.btnSendA.Click += new System.EventHandler(this.btnSendA_Click);
+            // 
+            // btnSendB
+            // 
+            this.btnSendB.Location = new System.Drawing.Point(13, 94);
+            this.btnSendB.Name = "btnSendB";
+            this.btnSendB.Size = new System.Drawing.Size(75, 23);
+            this.btnSendB.TabIndex = 3;
+            this.btnSendB.Text = "Recieve 2";
+            this.btnSendB.UseVisualStyleBackColor = true;
+            this.btnSendB.Click += new System.EventHandler(this.btnSendB_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(15, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(73, 23);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open Port";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(152, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close Port";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 120);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 6;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(127, 53);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 7;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(127, 94);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 20);
+            this.txt2.TabIndex = 8;
+            // 
+            // btnUpdate1
+            // 
+            this.btnUpdate1.Location = new System.Drawing.Point(257, 53);
+            this.btnUpdate1.Name = "btnUpdate1";
+            this.btnUpdate1.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate1.TabIndex = 9;
+            this.btnUpdate1.Text = "Update 1";
+            this.btnUpdate1.UseVisualStyleBackColor = true;
+            this.btnUpdate1.Click += new System.EventHandler(this.btnUpdate1_Click);
+            // 
+            // btnUpdate2
+            // 
+            this.btnUpdate2.Location = new System.Drawing.Point(257, 94);
+            this.btnUpdate2.Name = "btnUpdate2";
+            this.btnUpdate2.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate2.TabIndex = 10;
+            this.btnUpdate2.Text = "Update 2";
+            this.btnUpdate2.UseVisualStyleBackColor = true;
+            this.btnUpdate2.Click += new System.EventHandler(this.btnUpdate2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 152);
+            this.Controls.Add(this.btnUpdate2);
+            this.Controls.Add(this.btnUpdate1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSendB);
+            this.Controls.Add(this.btnSendA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSendA;
+        private System.Windows.Forms.Button btnSendB;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnUpdate1;
+        private System.Windows.Forms.Button btnUpdate2;
+    }
+}
+
---
+++ Arduino_Test/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Arduino_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        communicator comport = new communicator();
+        Boolean portConnection = false;
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //foreach (string port in SerialPort.GetPortNames()) // Adds all port names to ports list
+            //{
+            //    cbPorts.Items.Add(port); // adds ports to Ports combo-box
+            //}
+            //if (cbPorts.Items.Count > 0) // if there is at least one port available, select the             first one as default
+            //{
+            //    cbPorts.SelectedIndex = 0;
+            //    btnOpen.Enabled = true;
+            //}
+            //else // no ports available
+            //{
+            //    cbPorts.Text = "No Ports Available";
+            //    MessageBox.Show("Could not locate any available serial ports.", "No serial ports", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    //btnOpen.Enabled = false;
+            //}
+            //btnSendA.Enabled = false;
+            //btnSendB.Enabled = false;
+            //btnClose.Enabled = false;
+            txt1.Enabled = false;
+            txt2.Enabled = false;
+            btnSendA.Enabled = false;
+            btnSendB.Enabled = false;
+            btnClose.Enabled = false;
+        }
+
+        private void btnOpen_Click_1(object sender, EventArgs e)
+        {
+            if (comport.connect(9600, "I'M ARDUINO", 4, 8, 16))
+            {
+                lblStatus.Text = "Connection Successful - Connected to  " + comport.port;
+                portConnection = true;
+                //cbPorts.Enabled = false;
+                btnOpen.Enabled = false;
+                btnSendA.Enabled = true;
+                btnSendB.Enabled = true;
+                btnClose.Enabled = true;
+                txt1.Enabled = true;
+                txt2.Enabled = true;
+            }
+            else
+            {
+                lblStatus.Text = "Not connected . . . ";
+                portConnection = false;
+                //cbPorts.Enabled = true;
+                btnOpen.Enabled = true;
+                btnSendA.Enabled = false;
+                btnSendB.Enabled = false;
+                btnClose.Enabled = false;
+                txt1.Enabled = false;
+                txt2.Enabled = false;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            serialPort.Close();
+            lblStatus.Text = "Not connected . . . ";
+            portConnection = false;
+            // Change the Enabled flag for the buttons to prevent user error
+            //cbPorts.Enabled = true;
+            btnOpen.Enabled = true;
+            btnSendA.Enabled = false;
+            btnSendB.Enabled = false;
+            btnClose.Enabled = false;
+            txt1.Enabled = false;
+            txt2.Enabled = false;
+        }
+
+        private void btnSendA_Click(object sender, EventArgs e)
+        {
+            txt1.Text = comport.message(4, 8, 32);
+        }
+
+        private void btnSendB_Click(object sender, EventArgs e)
+        {
+            txt2.Text = comport.message(4, 8, 33);
+        }
+
+        private void btnUpdate1_Click(object sender, EventArgs e)
+        {
+
+            comport.send(4,8,34,txt1.Text, "Done");
+            txt1.Text = "";
+        }
+
+        private void btnUpdate2_Click(object sender, EventArgs e)
+        {
+            comport.send(4, 8, 35, txt2.Text, "Done");
+            txt2.Text = "";
+        }
+    }
+}
+
